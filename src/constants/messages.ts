ac3494d694ccaa74cb1f0fef14212109beda4d23
ikokywu <rikiridwan949@gmail.com>
2025-03-15 14:56:03 +0700

    feat: add bot response message

---
+++ src/constants/messages.ts
@@ -0,0 +1,14 @@
+const messages = {
+   about:
+     "_Bot ini dirancang untuk memungkinkan pengguna mengirim stiker dari Telegram ke WhatsApp dengan mudah. \n\nDeveloper: @rikkriuk_",
+   whatsAppInfo: `_Bot belum menemukan nomor WhatsApp kamu, silahkan masukkan nomor WhatsApp! \n\nContoh: 085722710523
+    _`,
+   invalidNumber:
+     "_Nomor tidak valid, silahkan masukkan nomor yang benar! \n\nContoh: 085722710523_",
+   validNumber: "_Nomor tersimpan📝 \nSilahkan kirim stiker!_",
+   stickerLimit:
+     "_Pengiriman stiker kamu telah mencapai batas, mohon tunggu beberapa menit!_",
+} as const;
+ 
+export default messages;
+ 
