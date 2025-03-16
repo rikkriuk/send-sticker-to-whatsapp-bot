6d77a5d628c9d76c4cedef706cc053f04f7f9754
ikokywu <rikiridwan949@gmail.com>
2025-03-16 16:31:50 +0700

    refactor: remove console

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI as string;
-console.log(MONGO_URI)
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
+    await mongoose.connect(MONGO_URI);
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
