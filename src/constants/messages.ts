b06352840440e6f6bbb9ed216488ec75c20c028d
rikkriuk <rikiridwan949@gmail.com>
2026-09-22 07:44:53 +0700

    fix: formatting

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -10,7 +10,7 @@ const messages = {
   about: `_@SendStickerBot merupakan bot yang bisa mengirimkan sticker Telegram ke WhatsApp secara otomatis lewat pesan.\n\nDibuat oleh: ${ADMIN_NAME} (@${ADMIN_TELEGRAM_USERNAME})_`,
   totalUsersInfo: (totalUsers: number, hasReviews: boolean = false) =>
     `Saat ini sudah ada *${totalUsers}* pengguna yang menikmati layanan ini${
-      hasReviews ? " (lihat review mereka /list_review)" : ""
+      hasReviews ? " (lihat review mereka /list\\_review)" : ""
     }.`,
   whatsAppInfo: `_Bot belum menemukan nomor WhatsApp kamu, silahkan tulis nomor WhatsApp! \n\nNomor yang kamu masukan akan menjadi tempat penerima stiker yang masuk lewat pesan.  \n\nContoh: 085722710523
   _`,
